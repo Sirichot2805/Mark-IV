c92f82b3920d95c4d4e408690e029f19f3ade285
Sirichot <Firstsirichot_2805@hotmail.com>
2022-06-07 01:21:41 +0700

    intial route questions

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { QuestionsModule } from './questions/questions.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, QuestionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
