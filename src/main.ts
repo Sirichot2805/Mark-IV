399852e0910531ddcb6fd82d49321f2615f8c9d8
Sirichot <Firstsirichot_2805@hotmail.com>
2022-07-05 10:54:12 +0700

    test port listen

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ||5000);
+  await app.listen(process.env.PORT ||5000, function() {
+    console.log('Listening on port %d', process.env.PORT);
+});
 }
 bootstrap();
